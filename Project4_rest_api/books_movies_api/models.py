8c5d433de3f558ebd7ffce98cc305f53370770dc
Conor Wantuch <cwantuch7@gmail.com>
2025-04-29 13:00:53 -0600

    making changes to data file

--- Project4_rest_api/books_movies_api/models.py
+++ Project4_rest_api/books_movies_api/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    date = models.DateField()
+    date = models.IntegerField()
     category = models.CharField(max_length=100)
     movie = models.ForeignKey('Movie', on_delete=models.SET_NULL, null=True, blank=True)
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    date = models.DateField()
+    date = models.IntegerField()
     budget = models.DecimalField(max_digits=12, decimal_places=2)
     actors = models.TextField()
     
