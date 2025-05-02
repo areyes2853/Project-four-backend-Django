c0e93dbb9aead2ae86fc8ce038a633fb3b3bc35c
Conor Wantuch <cwantuch7@gmail.com>
2025-05-02 12:18:16 -0600

    test

--- Project4_rest_api/books_movies_api/serializers.py
+++ Project4_rest_api/books_movies_api/serializers.py
@@ -10,7 +10,10 @@ class MovieSerializer(serializers.ModelSerializer):
 # Serializer for Book (includes the related movie)
 class BookSerializer(serializers.ModelSerializer):
     movie = MovieSerializer(read_only=True)
-
+    # Have to add this because
+    movie_id = serializers.PrimaryKeyRelatedField(
+        queryset=Movie.objects.all(), source='movie', write_only=True, allow_null=True
+    )
     class Meta:
         model = Book
-        fields = ['id', 'title', 'date', 'category', 'movie']
+        fields = ['id', 'title', 'date', 'category', 'movie', 'movie_id']
