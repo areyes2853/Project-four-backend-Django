42791044b8257296b8fe2552484a54dbca683492
areyes2853 <46231827+areyes2853@users.noreply.github.com>
2025-05-02 15:46:45 -0400

    Revert "Merge pull request #6 from areyes2853/angel"
    This reverts commit f0bc27413e0c64da72e439f687d5d9045fa4bcc7, reversing
    changes made to 29482292ea1a29128e7dd383d495beb1341c54fb.

--- Project4_rest_api/Project4_rest_api/settings.py
+++ Project4_rest_api/Project4_rest_api/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-s6jwc@n=l-ort1m+oqvjk_n_$veb_yu=&6-c8w#=x^uen=jtwl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+<<<<<<< HEAD
 ALLOWED_HOSTS = ['localhost','127.0.0.1', '3.83.236.184']
+=======
+ALLOWED_HOSTS = []
+>>>>>>> parent of f0bc274 (Merge pull request #6 from areyes2853/angel)
 
 
 # Application definition
@@ -91,10 +95,13 @@ DATABASES = {
         'NAME': 'bakendproject4_api',
         'USER': '',
         'PASSWORD': '',
+<<<<<<< HEAD
         'USER': '',
         'PASSWORD': '',
         'USER': '',
         'PASSWORD': '',
+=======
+>>>>>>> parent of f0bc274 (Merge pull request #6 from areyes2853/angel)
         'HOST': 'localhost'
     }
 }
